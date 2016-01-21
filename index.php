50ac148e078c9aa3f86054c148c19df4f21e7c24
TiepLe <TiepLe@users.noreply.github.com>
2016-01-21 21:42:18 +0700

    Update index.php

--- index.php
+++ index.php
@@ -5,8 +5,6 @@
 </head>
 <body>
 <h1> WebGis Giám Sát Độ Mặn Vùng Đồng Bằng Sông Cửu Long </h1>
-<img src="https://www.google.com.vn/search?q=logo+%C4%91%E1%BA%A1i+h%E1%BB%8Dc+c%C3%A2n+th%C6%A1&biw=1366&bih=641&source=lnms&tbm=isch&sa=X&sqi=2&ved=0ahUKEwiJhd2dj7vKAhVOHY4KHesCCbYQ_AUIBigB#imgrc=4iTmmQRtQ9xCDM%3A" 
-alt="Logo ĐHCT" title="Logo" width="500px" height="auto" />
 <iframe src="https://www.google.com/maps/d/u/0/embed?mid=zR72hlxQJR5k.kCWyt4CKyYVA" width="1040" height="580"></iframe>
 </body>
 </html>
