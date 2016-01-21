f9a6c6166498aaa0b29cc6b84a256fa06ad2a736
TiepLe <levutiep@gmail.com>
2016-01-21 10:06:03 +0700

    hi web

---
+++ index.php
@@ -0,0 +1,10 @@
+﻿<html xmln="http://www.w3.org/1999/xhtml">
+<head>
+<meta htpp-equiv="Content-Type" content="text/html; charset=utf-8" />
+<title>Home Page</title>
+<meta htpp-equiv="content-language"> content="en-us" />
+</head>
+<body>
+<h1> Chào mừng đến với website của Tiệp! </h1>
+</body>
+</html>
