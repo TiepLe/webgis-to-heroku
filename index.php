ab7b90c8a7d5c69c3371c708aa63dbd1fa9e3b9e
TiepLe <TiepLe@users.noreply.github.com>
2016-01-21 21:35:49 +0700

    Update index.php

--- index.php
+++ index.php
@@ -1,10 +1,12 @@
 ﻿<html xmln="http://www.w3.org/1999/xhtml">
 <head>
 <meta htpp-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Home Page</title>
+<title>Trang Chủ</title>
 </head>
 <body>
 <h1> WebGis Giám Sát Độ Mặn Vùng Đồng Bằng Sông Cửu Long </h1>
- <iframe src="https://www.google.com/maps/d/u/0/embed?mid=zR72hlxQJR5k.kCWyt4CKyYVA" width="1040" height="580"></iframe>
+<img src="https://www.google.com.vn/search?q=logo+%C4%91%E1%BA%A1i+h%E1%BB%8Dc+c%C3%A2n+th%C6%A1&biw=1366&bih=641&source=lnms&tbm=isch&sa=X&sqi=2&ved=0ahUKEwiJhd2dj7vKAhVOHY4KHesCCbYQ_AUIBigB#imgrc=4iTmmQRtQ9xCDM%3A" 
+alt="Logo ĐHCT" title="Logo" width="500px" height="auto" />
+<iframe src="https://www.google.com/maps/d/u/0/embed?mid=zR72hlxQJR5k.kCWyt4CKyYVA" width="1040" height="580"></iframe>
 </body>
 </html>
